8d2743eb45e2450f4bded700958c4913083ed1a2
worksigithub <37853670+worksigithub@users.noreply.github.com>
2022-06-17 10:53:33 -0300

    Datos acceso bd

--- api/model/modelDatos.php
+++ api/model/modelDatos.php
@@ -1,11 +1,11 @@
 <?php
- $db_host = "127.0.0.1";
+ /* $db_host = "127.0.0.1";
  $db_base = "worksiSoporte";
  $db_user = "root";
- $db_pass = "";
+ $db_pass = ""; */
 
- /*  $db_host = "localhost";
- $db_base = "c2backoffice";
- $db_user = "c2zamorano";
- $db_pass = "LxD!K95wb"; */
+ $db_host = "localhost";
+ $db_base = "c2wks";
+ $db_user = "c2wks";
+ $db_pass = "D@gxF9ZrqvSvH"; 
 ?>
