9d811327bf3dda73f4bc53e6da8988ecd79980ea
benjahoff <oriozabalabenja@gmail.com>
2022-06-24 14:03:27 -0300

    token

--- api/controller/ticketController.php
+++ api/controller/ticketController.php
@@ -130,8 +130,7 @@ class ticketController extends crudController
         ];
 
         $headr = array();
-        $headr[] =  'Authorization: Bearer EAAKnnxNa2QoBAE6MuZBiDZC5bXrPJOZCBHZAfKGZB0NdifctJg6wWXbzxrX1lVxqqSQfcPl7A7nzZCCZAJTKKIdKWmdS0NUNcPdqKcUhOn4YlZAkh9cQZANNyZAwoTi2q8A6ZCqgknBeII9fgWLj0pr0hmvOKWYzAICKI6994ul1k1oMLT6ZAVNFZAbO4';
-        # Create a connection
+        $headr[] =  'Authorization: Bearer EAAKnnxNa2QoBAE6MuZBiDZC5bXrPJOZCBHZAfKGZB0NdifctJg6wWXbzxrX1lVxqqSQfcPl7A7nzZCCZAJTKKIdKWmdS0NUNcPdqKcUhOn4YlZAkh9cQZANNyZAwoTi2q8A6ZCqgknBeII9fgWLj0pr0hmvOKWYzAICKI6994ul1k1oMLT6ZAVNFZAbO4';        # Create a connection
         $url = 'https://graph.facebook.com/v13.0/101900062567452/messages';
         //$url = 'localhost/worksi-web/generate-electronic-billing';
         $ch = curl_init($url);
